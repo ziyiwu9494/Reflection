8fa969cd49bad2a4edaabc9c129c568f71235e61
Ziyi Wu <ziyiwu@usc.edu>
2020-03-01 02:39:09 -0800

    basic movement

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    Vector2 p;
+    // Start is called before the first frame update
+    void Start()
+    {
+       p = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        p.x = player.position.x;
+        transform.position = p;
+    }
+}
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -9,20 +9,36 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0;
     public float runSpeed = 40f;
     bool jump = false;
+    bool hit = false;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("Jump", true);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            hit = true;
+            animator.SetBool("Hit", true);
         }
     }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jump", false);
+    }   
 
     private void FixedUpdate()
     {
         controller.Move(horizontalMove*Time.fixedDeltaTime, false, jump);
-        jump = false; 
+        jump = false;
+        animator.SetBool("Hit", false);
     }
+    
 }
