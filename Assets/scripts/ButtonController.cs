e17df1776209225e563c851faa66bfcfcde24873
Ziyi Wu <ziyiwu@usc.edu>
2020-03-01 16:38:37 -0800

    almost done

---
+++ Assets/scripts/ButtonController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public Animator animator;    
+    // Start is called before the first frame update
+    public void ButtonClicked()
+    {
+        animator.SetBool("Begin", true);   
+    }
+}
